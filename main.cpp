8404c2ae7fd9e2b8ff5f312985af8bd1506baadd
SobaMask3 <newbie.721nation@gmail.com>
2021-02-23 22:07:24 +0200

    mai.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+int main(void){
+
+string myText;
+
+ifstream MyReadFile("filename.txt");
+
+
+while(getline(MyReadFile,myText)){
+
+
+}
+
+
+
+
+
+
+return 0;
+}
