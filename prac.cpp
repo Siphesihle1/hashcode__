95921c1508a9e71bb425d453a9d906c15c9a9c7e
Siphesihle Mthethwa <percysiphe@gmail.com>
2021-02-24 20:42:49 +0200

    ***

--- prac.cpp
+++ prac.cpp
@@ -2,48 +2,57 @@
 #include<fstream>
 #include<vector>
 #include<sstream>
+
 using namespace std;
 
-vector<int> sumfunc(vector<int>ipt,int target){
-  vector<int>final;
-  //first  number we add
-  int sindex = 1;
-  //first number in the add
-  int findex= 0;
+int getsum(vector<int> v)
+{
+	int psum = 0;
+	for (int i : v)
+	{
+		psum += i;
+	}
+	return psum;
+}
 
-  int sum = ipt[findex];
+vector<int> sumfunc(vector<int>ipt,int target)
+{
+	
+	vector<int>final;
+	
+	if (getsum(ipt) < target) return ipt;
 
-  for(int k = 0;k<ipt.size();k++)
-  {
-    for (int i = findex; i < ipt.size(); i++)
-    {
-      for (int j = sindex; j <ipt.size(); j++)
-      {
-            if(sum<target)
-            {
-              sum+=ipt[j];
-              final.push_back(ipt[j]);
-              
-            }
-            else if(sum >target)
-            {
-              sindex++;
-              final.erase(final.begin(),final.begin()+final.size()-1);
-              break;
+	for(int findex = 0; findex < ipt.size(); findex++)
+	{
+		for (int sindex = findex + 1; sindex < ipt.size(); sindex++)
+		{
+			int sum = ipt[findex];
+			final.push_back(ipt[findex]);
+			for (int j = sindex; j < ipt.size(); j++)
+			{
+				if(sum < target)
+				{
+					sum += ipt[j];
+					final.push_back(ipt[j]);
 
-            }
-            else
-            {
-              return final;
-            }
+				}
+				else if (sum > target)
+				{
+					final.erase(final.begin(), final.begin() + final.size());
+					sum = 0;
+					break;
 
-      }
-    }
+				}
+				else
+				{
+					return final;
+				}
 
-  }
+			}
+		}
 
-
-  return final;
+	}
+	return {};
 }
 
 
@@ -53,24 +62,18 @@ vector<int> sumfunc(vector<int>ipt,int target){
 
 int main(void)
 {
-  int sun;
-  vector<int> v = {2,2,3,3,3,4};
-  vector<int> v2;
-  cout<<"ENter SUm"<<endl;
-  cin>>sun;
+	int sum;
+	vector<int> v = {2, 2, 3, 3, 3, 4};
+	vector<int> v2;
+	cout << "Enter sum" << endl;
+	cin>>sum;
 
-  v2 = sumfunc(v,sun);
+	v2 = sumfunc(v, sum);
 
-  for (size_t i = 0; i< v2.size(); i++) {
-    cout<<v2[i]<<endl;
+	for (size_t i = 0; i < v2.size(); i++) 
+	{
+		cout << v2[i] << endl;
+	}
 
-  }
-
-
-
-
-
-
-return 0;
-
+	return 0;
 }
